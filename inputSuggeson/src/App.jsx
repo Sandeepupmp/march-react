23901bff5fe7efea3f06809a03914cf0ec6654db
sandeep <sandeepupmp@gmail.com>
2024-03-22 15:36:03 +0530

    input suggeson project going to setup

---
+++ inputSuggeson/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="cantainer">
+      <h1>Auto Suggestion Box</h1>
+      </div> 
+    </>
+  )
+}
+
+export default App
